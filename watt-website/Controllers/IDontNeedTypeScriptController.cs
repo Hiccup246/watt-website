9f8f79641ca17c815f301e15f746685969aa9fbc
James Watt <james.watt529@hotmail.co.nz>
2020-03-15 16:28:50 +1300

    init project

---
+++ watt-website/Controllers/IDontNeedTypeScriptController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace watt_website.Controllers
+{
+    public class IDontNeedTypeScriptController : Controller
+    {
+        /// <summary>
+        /// SPA entry point
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ watt-website/Controllers/TemplateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace watt_website.Controllers
+{
+    public class TemplateController : Controller
+    {
+        /// <summary>
+        /// SPA entry point
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
